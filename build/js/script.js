c33fffa82daea860840e0076789e7c27499fb7ff
Vitalii Zaliva <neapolvet@gmail.com>
2021-07-31 23:23:35 +0300

    Build v1

--- build/js/script.js
+++ build/js/script.js
@@ -1,52 +1,23 @@
-$(document).ready(function() {
-    $('.slider-items').slick({
-        infinite: true,
-        slidesToShow: 6,
-        slidesToScroll: 1,
-        nextArrow: '<img src="img/slick-items/next.svg" class="next-arrow">',
-        prevArrow: '<img src="img/slick-items/prev.svg" class="prev-arrow">',
-        responsive: [
-            {
-                breakpoint: 1100,
-                settings: {
-                    slidesToShow: 4,
-                    slidesToScroll: 2,
-                    infinite: true,
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 2
-                }
-            },
-            {
-                breakpoint: 620,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
-    });
-    $('.header-burger').click(function(event) {
-        $('.header-burger').toggleClass('active');
-        $(".main-nav").toggleClass('visible');
+window.addEventListener('load', function() {
+    let headerOpenBtn = document.querySelector('.header__toggle-btn');
+    let headerCloseBtn = document.querySelector('.header__nav-btn');
+    let headerMenu = document.querySelector('.header__nav');
+
+    headerOpenBtn.addEventListener('click', function() {
+        headerMenu.classList.add('active');
     });
 
-    $('.campaign-tab').click(function() {
-        $('.campaign-image').hide();
-        $('.campaign-tab').removeClass('active');
-        $(this).addClass('active');
-        var attr_name = $(this).attr('data-list');
-        $('.'+attr_name).show();
+    headerCloseBtn.addEventListener('click', function() {
+        headerMenu.classList.remove('active');
     });
-})
+
+    $('.banner').slick({
+        infinite: true,
+        slidesToShow: 1,
+        arrows: false,
+        slidesToScroll: 1,
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 4000
+    });
+});
