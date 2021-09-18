db29b306fdc7fe0352738356c3fe8f73d96dc7f3
Vitalii Zaliva <neapolvet@gmail.com>
2021-09-18 18:46:39 +0300

    Fixed admin issue

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/css');
     eleventyConfig.addPassthroughCopy('./src/js');
     eleventyConfig.addPassthroughCopy('./src/img');
+    eleventyConfig.addPassthroughCopy('./src/admin');
 
      return {
          dir: {
